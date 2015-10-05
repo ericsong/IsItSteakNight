02ef3c7c33970f3532b71343d76214da4ff90b30
ericsong <regonics@gmail.com>
2015-10-05 14:23:26 -0400

    only saves if new menu is found

--- save.js
+++ save.js
@@ -7,13 +7,43 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 var menuSchema = mongoose.Schema({
-    data: Object
+    data: Object,
+    time : { type : Date, default: Date.now } 
 });
 
 var Menu = mongoose.model('menu', menuSchema);
 
 request('https://rumobile.rutgers.edu/1/rutgers-dining.txt', function(error, response, body) {
-    var newMenu = new Menu({data: JSON.parse(body)});
+    var menudata = JSON.parse(body);
+    Menu.findOne({}, {}, {sort: {'time': -1} }, function(err, doc) {
+        if(!doc) {
+            var newMenu = new Menu({data: JSON.parse(body)});
 
-    newMenu.save()
+            newMenu.save()
+            console.log('saved menu');
+            return;
+        }
+
+        var isSame = true;
+
+        for(var i = 0; i < doc.data.length; i++) {
+            var hall = doc.data[i].location_name;
+            var time = doc.data[i].date;
+
+            for(var j = 0; j < menudata.length; j++) {
+                if(menudata[j].location_name == hall) {
+                    if(time != menudata[j].date) {
+                        isSame = false;
+                    }
+                }
+            }
+        }
+
+        if(!isSame) {
+            var newMenu = new Menu({data: JSON.parse(body)});
+
+            newMenu.save()
+            console.log('saved menu');
+        }
+    })
 });
