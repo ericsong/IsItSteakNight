6eada92f22be0b2bed8d7750d4e9ad4760d71225
ericsong <regonics@gmail.com>
2015-10-06 16:04:50 -0400

    print out all genres using es6 babel

---
+++ indexer.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var request = require('request');
+
+mongoose.connect('mongodb://localhost/isitsteaknight');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var menuSchema = mongoose.Schema({
+    data: Object,
+    time : { type : Date, default: Date.now } 
+});
+
+var Menu = mongoose.model('menu', menuSchema);
+
+Menu.find(function(err, docs) {
+    for(var i = 0; i < docs.length; i++) {
+        for(var j = 0; j < docs[i].data.length; j++) {
+            var busch_menu;
+
+            if(docs[i].data[j].location_name = 'Busch Dining Hall') {
+                busch_menu = docs[i].data[j].meals;
+            }
+
+            for(let meal of busch_menu) {
+                if(meal.genres) {
+                    for(let genre of meal.genres) {
+                        console.log(genre)
+                    }
+                }
+            }
+        }
+    }
+})
