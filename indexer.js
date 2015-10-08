e73c93bfcca9c802db6da93e5d5051ac452c1393
ericsong <regonics@gmail.com>
2015-10-07 20:00:07 -0400

    indexes all items into db

--- indexer.js
+++ indexer.js
@@ -11,26 +11,45 @@ var menuSchema = mongoose.Schema({
     time : { type : Date, default: Date.now } 
 });
 
+var itemSchema = mongoose.Schema({
+    hall: String,
+    category: String,
+    item: String
+});
+
 var Menu = mongoose.model('menu', menuSchema);
+var Item = mongoose.model('item', itemSchema);
 
 Menu.find(function(err, docs) {
     for(var i = 0; i < docs.length; i++) {
         for(var j = 0; j < docs[i].data.length; j++) {
-            var busch_menu;
+            var location_name = docs[i].data[j].location_name;
+            var menu = docs[i].data[j].meals
 
-            if(docs[i].data[j].location_name = 'Busch Dining Hall') {
-                busch_menu = docs[i].data[j].meals;
-            }
-
-            for(let meal of busch_menu) {
+            for(let meal of menu) {
                 if(meal.genres) {
                     for(let genre of meal.genres) {
                         for(let item of genre.items) {
-                            console.log(genre.genre_name + ': ' + item)
+                            Item.findOne({
+                                hall: location_name,
+                                category: genre.genre_name,
+                                item: item
+                            }, function(err, doc) {
+                                console.log(doc)
+                                if(!doc) {
+                                    console.log(genre.genre_name + ': ' + item)
+                                    var newItem = new Item({
+                                        hall: location_name,
+                                        category: genre.genre_name,
+                                        item: item
+                                    }).save()
+                                } 
+                            })
                         }
                     }
                 }
             }
+
         }
     }
 })
