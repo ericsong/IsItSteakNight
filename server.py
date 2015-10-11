d1657db60fbb13628dff9cdab06a88181bf40999
ericsong <regonics@gmail.com>
2015-10-11 09:38:53 -0400

    add caching for isSteakNight

--- server.py
+++ server.py
@@ -1,11 +1,27 @@
+import threading
 from flask import Flask
 from analyzer import isTonightSteakNight
 
 app = Flask(__name__)
 
+isSteakNight = False
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+def updateSteakCheck():
+    isSteakNight = isTonightSteakNight()
+
 @app.route('/')
 def root():
-    return str(isTonightSteakNight())
+    return str(isSteakNight)
 
 if __name__ == '__main__':
+    updateSteakCheck()
+    set_interval(updateSteakCheck, 60)
     app.run()
