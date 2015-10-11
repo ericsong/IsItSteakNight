4115541ae893a04dba67d5ad10b83c007eee488e
ericsong <regonics@gmail.com>
2015-10-10 21:50:09 -0400

    add user queries

--- psql.sql
+++ psql.sql
@@ -1,3 +1,6 @@
+CREATE ROLE iisn_admin WITH PASSWORD 'abc123';
+GRANT ALL PRIVILEGES ON DATABASE isitsteaknight TO iisn_admin;
+
 CREATE TABLE "DiningHall" ("id" SERIAL PRIMARY KEY, "name" TEXT);
 CREATE TABLE "Genre" ("id" SERIAL PRIMARY KEY, "name" TEXT, "dininghall" INT references "DiningHall"(id));
 CREATE TABLE "Item" ("id" SERIAL PRIMARY KEY, "name" TEXT, "genre" INT references "Genre"(id));
