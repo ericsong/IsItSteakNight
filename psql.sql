b93318f5300bed9060740232d4d341b5dfe20bce
ericsong <regonics@gmail.com>
2015-10-10 21:14:59 -0400

    add queries to add dining hall entries

--- psql.sql
+++ psql.sql
@@ -1,3 +1,8 @@
 CREATE TABLE "DiningHall" ("id" SERIAL PRIMARY KEY, "name" TEXT);
 CREATE TABLE "Genre" ("id" SERIAL PRIMARY KEY, "name" TEXT, "dininghall" INT references "DiningHall"(id));
 CREATE TABLE "Item" ("id" SERIAL PRIMARY KEY, "name" TEXT, "genre" INT references "Genre"(id));
+
+INSERT INTO "DiningHall" (name) VALUES ('Brower Commons');
+INSERT INTO "DiningHall" (name) VALUES ('Busch Dining Hall');
+INSERT INTO "DiningHall" (name) VALUES ('Livingston Dining Commons');
+INSERT INTO "DiningHall" (name) VALUES ('Neilson Dining Hall');
