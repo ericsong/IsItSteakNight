971741d02aaf7214cc6ee161dd50f99164502fb0
ericsong <regonics@gmail.com>
2015-10-24 12:09:51 -0400

    better name for sql setup

---
+++ db_setup.sql
@@ -0,0 +1,18 @@
+CREATE ROLE iisn_admin WITH PASSWORD 'abc123';
+ALTER ROLE iisn_admin WITH LOGIN;
+
+GRANT ALL PRIVILEGES ON DATABASE isitsteaknight TO iisn_admin;
+GRANT ALL PRIVILEGES ON TABLE "DiningHall" TO iisn_admin;
+GRANT ALL PRIVILEGES ON TABLE "Genre" TO iisn_admin;
+GRANT ALL PRIVILEGES ON TABLE "Item" TO iisn_admin;
+GRANT ALL PRIVILEGES ON sequence "Genre_id_seq" TO iisn_admin;
+GRANT ALL PRIVILEGES ON sequence "Item_id_seq" TO iisn_admin;
+
+CREATE TABLE "DiningHall" ("id" SERIAL PRIMARY KEY, "name" TEXT);
+CREATE TABLE "Genre" ("id" SERIAL PRIMARY KEY, "name" TEXT, "dininghall" INT references "DiningHall"(id));
+CREATE TABLE "Item" ("id" SERIAL PRIMARY KEY, "name" TEXT, "genre" INT references "Genre"(id));
+
+INSERT INTO "DiningHall" (name) VALUES ('Brower Commons');
+INSERT INTO "DiningHall" (name) VALUES ('Busch Dining Hall');
+INSERT INTO "DiningHall" (name) VALUES ('Livingston Dining Commons');
+INSERT INTO "DiningHall" (name) VALUES ('Neilson Dining Hall');
