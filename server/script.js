d729d4171ba5ba098e5ba596511edb9d6cead95d
ericsong <regonics@gmail.com>
2015-10-28 23:48:04 -0400

    finish converting analyzer to JS

--- server/script.js
+++ server/script.js
@@ -12,6 +12,8 @@ function validGenre(genre) {
 }
 
 function miscFlags(item) {
+    var item_lower = item.toLowerCase();
+
     if(
         item_lower.indexOf("philly") > 0 ||
         item_lower.indexOf("tuna") > 0 ||
@@ -23,49 +25,47 @@ function miscFlags(item) {
     return true;
 }
 
-def miscFlags(item):
-    item_lower = item.lower()
-    if "philly" in item_lower or "tuna" in item_lower or "sandwich" in item_lower:
-        return False
+function isSteakItem(genre, item) {
+    var item_lower = item.toLowerCase();
 
-    return True
+    if(
+        item_lower.indexOf("steakk") > 0 &&
+        validGenre(genre) &&
+        miscFlags(item)
+    ) {
+        return true;
+    }
 
-def isSteakItem(genre, item):
-    item_lower = item.lower()
-    if "steak" in item_lower and validGenre(genre) and miscFlags(item):
-        return True
+    return false;
+}
 
-    return False
+//rename this to get steak items from menu
+function checkIfMenuHasSteak(menu) {
+    var hasSteak = false;
+    var items = [];
 
-def checkIfMenuHasSteak(menu):
-    hasSteak = False
-    items = []
+    for(var dininghall of menu) {
+        for(var meal of dininghall.meals) {
+            if(!meal.meal_avail) {
+                return items;
+            }
 
-    for dininghall in menu:
-        for meal in dininghall['meals']:
-            if(meal['meal_avail']):
-                for genre in meal['genres']:
-                    for item in genre['items']:
-                        if isSteakItem(genre['genre_name'], item):
-                            hasSteak = True
-                            items.append({
-                                'dininghall': dininghall['location_name'],
-                                'meal': meal['meal_name'],
-                                'genre': genre['genre_name'],
-                                'item': item
-                            })
+            for(var genre of meal.genres) {
+                for(var item of genre.items) {
+                    if(isSteakItem(genre.genre_name, item)) {
+                        hasSteak = true;
 
-    #return items here
-    return (hasSteak, items)
+                        items.push({
+                            dininghall: dininghall.location_name,
+                            meal: meal.meal_name,
+                            genre: genre.genre_name,
+                            item: item  
+                        })
+                    } 
+                }
+            }
+        }
+    }
 
-def isTonightSteakNight(queryDatabase=False):
-    if queryDatabase:
-        latestMenu = menus.find().sort([("time", pymongo.DESCENDING)])[0]['data']
-    else:
-        r = requests.get('https://rumobile.rutgers.edu/1/rutgers-dining.txt')
-        latestMenu = json.loads(r.text)
-
-    unix_time = latestMenu[0]['date'] / 1000
-    menutime = time.localtime(unix_time)
-    date_string = time.strftime("%B %d, %Y",  menutime)
-    return checkIfMenuHasSteak(latestMenu)
+    return items;
+}
