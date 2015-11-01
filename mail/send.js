091c88342a52656073f54ae7285da52c3887aded
ericsong <regonics@gmail.com>
2015-11-01 11:05:18 -0500

    send test email (single)

--- mail/send.js
+++ mail/send.js
@@ -1,9 +1,24 @@
 import Client from 'pg-native'
 import request from 'request'
+import Sendgrid from 'sendgrid'
 import getMatchingItems from './analyzer'
 
-let client = new Client()
+var client = new Client()
 client.connectSync('postgresql://iisn_admin:abc123@localhost:5432/isitsteaknight')
+
+var sendgrid = Sendgrid(process.env.SG_USER, process.env.SG_KEY)
+var email = new sendgrid.Email()
+
+var payload = {
+    to: "regonics@gmail.com",
+    from: "me@isitsteaknight.com",
+    subject: "Steak is being served at Busch Dining Hall",
+    text: "Teriyaki steak is being served at Busch Dining Hall for Dinner"
+}
+sendgrid.send(payload, (err, json) => {
+    if (err) { console.error(err) }
+    console.log(json)
+})
 
 request('https://rumobile.rutgers.edu/1/rutgers-dining.txt', (error, body, response) => {
     let menu = JSON.parse(body.body)
@@ -14,6 +29,7 @@ request('https://rumobile.rutgers.edu/1/rutgers-dining.txt', (error, body, respo
         let query = rows[i].query;
         let emails = [];
 
+        let matchedItems = getMatchingItems(menu, query)
         while(rows[i].query.toLowerCase() == query.toLowerCase()) {
             emails.push(rows[i].email)
             i++
@@ -25,6 +41,6 @@ request('https://rumobile.rutgers.edu/1/rutgers-dining.txt', (error, body, respo
         i--
 
         console.log(query + ": " + emails)
-        console.log(getMatchingItems(menu, query))
+        console.log(matchedItems)
     }
 });
