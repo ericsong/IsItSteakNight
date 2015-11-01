0b8cae50cd6565bcec75571df8a9d2674de872b9
ericsong <regonics@gmail.com>
2015-10-31 22:17:17 -0400

    create relations before editting

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -1,17 +1,17 @@
 CREATE ROLE iisn_admin WITH PASSWORD 'abc123';
 ALTER ROLE iisn_admin WITH LOGIN;
 
+CREATE TABLE "DiningHall" ("id" SERIAL PRIMARY KEY, "name" TEXT);
+CREATE TABLE "Genre" ("id" SERIAL PRIMARY KEY, "name" TEXT, "dininghall" INT references "DiningHall"(id));
+CREATE TABLE "Item" ("id" SERIAL PRIMARY KEY, "name" TEXT, "genre" INT references "Genre"(id));
+CREATE TABLE "Subscription" ("id" SERIAL PRIMARY KEY, "email" TEXT, "query" TEXT)
+
 GRANT ALL PRIVILEGES ON DATABASE isitsteaknight TO iisn_admin;
 GRANT ALL PRIVILEGES ON TABLE "DiningHall" TO iisn_admin;
 GRANT ALL PRIVILEGES ON TABLE "Genre" TO iisn_admin;
 GRANT ALL PRIVILEGES ON TABLE "Item" TO iisn_admin;
 GRANT ALL PRIVILEGES ON sequence "Genre_id_seq" TO iisn_admin;
 GRANT ALL PRIVILEGES ON sequence "Item_id_seq" TO iisn_admin;
-
-CREATE TABLE "DiningHall" ("id" SERIAL PRIMARY KEY, "name" TEXT);
-CREATE TABLE "Genre" ("id" SERIAL PRIMARY KEY, "name" TEXT, "dininghall" INT references "DiningHall"(id));
-CREATE TABLE "Item" ("id" SERIAL PRIMARY KEY, "name" TEXT, "genre" INT references "Genre"(id));
-CREATE TABLE "Subscription" ("id" SERIAL PRIMARY KEY, "email" TEXT, "query" TEXT)
 
 INSERT INTO "DiningHall" (name) VALUES ('Brower Commons');
 INSERT INTO "DiningHall" (name) VALUES ('Busch Dining Hall');
