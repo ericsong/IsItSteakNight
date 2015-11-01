7691004305f7574cc98eaf782c664df5a1110391
ericsong <regonics@gmail.com>
2015-11-01 01:59:51 -0500

    running analyzer on email batcher

--- mail/send.js
+++ mail/send.js
@@ -1,7 +1,30 @@
 import Client from 'pg-native'
+import request from 'request'
+import getMatchingItems from './analyzer'
 
 let client = new Client()
 client.connectSync('postgresql://iisn_admin:abc123@localhost:5432/isitsteaknight')
 
-var rows = client.querySync('SELECT * FROM "Subscription"');
-console.log(rows)
+request('https://rumobile.rutgers.edu/1/rutgers-dining.txt', (error, body, response) => {
+    let menu = JSON.parse(body.body)
+    let rows = client.querySync('SELECT query, email FROM "Subscription" ORDER BY query');
+    
+    console.log(rows)
+    for(let i = 0; i < rows.length; i++) {
+        let query = rows[i].query;
+        let emails = [];
+
+        while(rows[i].query.toLowerCase() == query.toLowerCase()) {
+            emails.push(rows[i].email)
+            i++
+
+            if(i == rows.length) {
+                break
+            }
+        }
+        i--
+
+        console.log(query + ": " + emails)
+        console.log(getMatchingItems(menu, query))
+    }
+});
