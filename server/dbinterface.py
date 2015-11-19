4239a25d52d0714ae72da46c1b90f14dda2942ac
ericsong <regonics@gmail.com>
2015-11-18 23:40:20 -0500

    move db functions into sep file

---
+++ server/dbinterface.py
@@ -0,0 +1,172 @@
+import psycopg2
+import uuid
+import string
+import os
+from raven import Client as RavenClient
+
+DB_PASS = os.environ['IISN_DB_PASS']
+
+raven_client = RavenClient(os.environ['SENTRY_DSN'])
+
+try:
+    conn = psycopg2.connect("host='localhost' dbname='isitsteaknight' user='iisn_admin' password='" + DB_PASS + "'")
+except:
+    print("I am unable to connect to the database")
+
+status = {
+    'isSteakNight': False
+}
+
+def tryCatchSelectOneQuery(query):
+    cur = conn.cursor()
+
+    try:
+        cur.execute(query)
+        doc = cur.fetchone()
+
+        cur.close()
+        return doc
+    except:
+        raven_client.user_context({
+            'query': query
+        })
+        raven_client.captureMessage('PostgreSQL SELECT query failed')
+
+        cur.close()
+
+        raise ValueError("PostgreSQL SELECT query failed")
+        return None
+
+def tryCatchInsertQuery(query):
+    cur = conn.cursor()
+
+    try:
+        cur.execute(query)
+        conn.commit()
+        cur.close()
+
+        return True
+    except:
+        raven_client.user_context({
+            'query': query
+        })
+        raven_client.captureMessage('PostgreSQL INSERT query failed')
+
+        cur.close()
+        
+        raise ValueError("PostgreSQL INSERT query failed")
+        return None
+
+def getSubscription(email, query):
+    """ Returns subscription if exists """
+
+    values = {
+        'email': email,
+        'query': query
+    }
+
+    select_template = string.Template("""
+        SELECT id from "Subscription" WHERE subscriber='$email' AND query='$query'
+    """)
+    select_query = select_template.substitute(values)
+
+    return tryCatchSelectOneQuery(select_query)
+
+def getSubscriberByKey(key):
+    """ Returns subscriber if exists """
+
+    values = {
+        'key': key
+    }
+
+    select_template = string.Template("""
+        SELECT email from "Subscriber" WHERE key='$key'
+    """)
+    select_query = select_template.substitute(values)
+
+    return tryCatchSelectOneQuery(cur, select_query)
+
+def getSubscriberByEmail(email):
+    """ Returns subscriber if exists """
+
+    values = {
+        'email': email
+    }
+
+    select_template = string.Template("""
+        SELECT email from "Subscriber" WHERE email='$email'
+    """)
+    select_query = select_template.substitute(values)
+
+    return tryCatchSelectOneQuery(cur, select_query)
+
+def subscriptionExists(email, query):
+    """ Return if subscription exists or not """
+
+    result = getSubscription(email, query)
+    
+    if result is not None:
+        return True
+    else:
+        return False
+
+def subscriberExists(key):
+    """ Return if subscriber exists or not """
+
+    result = getSubscriberByKey(key)
+
+    if result is not None:
+        return True
+    else:
+        return False
+
+def createSubscriber(email):
+    """ Create a subscriber and add to database. Return his unique key """
+
+    values = {
+        'email': email,
+        'key': uuid.uuid4(),
+    }
+
+    insert_template = string.Template("""
+        INSERT INTO "Subscriber" (email, key) VALUES ('$email', '$key')
+    """)
+    insert_query = insert_template.substitute(values)
+    result = tryCatchInsertQuery(insert_query)
+
+    if result is not None:
+        return values['key']
+    else:
+        return None
+
+def createSubscription(email, query):
+    """ Create a subscription and add to database. Return unique id if successful """
+
+    values = {
+        'email':  email,
+        'query': query
+    }
+
+    insert_template = string.Template("""
+        INSERT INTO "Subscription" (subscriber, query) VALUES ('$email', '$query')
+    """)
+    insert_query = insert_template.substitute(values)
+    result = tryCatchInsertQuery(cur, insert_query)
+
+    return result
+
+def updateSubscriberConfirmStatus(key, status):
+    """ Set subscriber with key of key as confirmed """
+
+    values = {
+        'key': key,
+        'status': status
+    }
+
+    update_template = string.Template("""
+        UPDATE "Subscriber" SET verified=$status WHERE key='$token'
+    """)
+    update_query = update_template.substitute(values)
+    return tryCatchInsertQuery(cur, update_query)
+
+
--- server/server.py
+++ server/server.py
@@ -1,183 +1,19 @@
 import string
 import threading
-import psycopg2
 import re
-import uuid
-import os
-from raven import Client as RavenClient
+import dbinterface
 from flask import Flask, render_template, send_from_directory, request, jsonify
 from analyzer import isTonightSteakNight, getMenu
 from send import sendConfirmationEmail
-
-DB_PASS = os.environ['IISN_DB_PASS']
-
-raven_client = RavenClient(os.environ['SENTRY_DSN'])
 
 app = Flask(__name__, static_url_path='')
 app.debug = True
 
 pattern = re.compile(".+@.+\..+")
 
-try:
-    conn = psycopg2.connect("host='localhost' dbname='isitsteaknight' user='iisn_admin' password='" + DB_PASS + "'")
-except:
-    print("I am unable to connect to the database")
-
 status = {
     'isSteakNight': False
 }
-
-def tryCatchSelectOneQuery(query):
-    cur = conn.cursor()
-
-    try:
-        cur.execute(query)
-        doc = cur.fetchone()
-
-        cur.close()
-        return doc
-    except:
-        raven_client.user_context({
-            'query': query
-        })
-        raven_client.captureMessage('PostgreSQL SELECT query failed')
-
-        cur.close()
-
-        raise ValueError("PostgreSQL SELECT query failed")
-        return None
-
-def tryCatchInsertQuery(query):
-    cur = conn.cursor()
-
-    try:
-        cur.execute(query)
-        conn.commit()
-        cur.close()
-
-        return True
-    except:
-        raven_client.user_context({
-            'query': query
-        })
-        raven_client.captureMessage('PostgreSQL INSERT query failed')
-
-        cur.close()
-        
-        raise ValueError("PostgreSQL INSERT query failed")
-        return None
-
-def getSubscription(email, query):
-    """ Returns subscription if exists """
-
-    values = {
-        'email': email,
-        'query': query
-    }
-
-    select_template = string.Template("""
-        SELECT id from "Subscription" WHERE subscriber='$email' AND query='$query'
-    """)
-    select_query = select_template.substitute(values)
-
-    return tryCatchSelectOneQuery(select_query)
-
-def getSubscriberByKey(key):
-    """ Returns subscriber if exists """
-
-    values = {
-        'key': key
-    }
-
-    select_template = string.Template("""
-        SELECT email from "Subscriber" WHERE key='$key'
-    """)
-    select_query = select_template.substitute(values)
-
-    return tryCatchSelectOneQuery(cur, select_query)
-
-def getSubscriberByEmail(email):
-    """ Returns subscriber if exists """
-
-    values = {
-        'email': email
-    }
-
-    select_template = string.Template("""
-        SELECT email from "Subscriber" WHERE email='$email'
-    """)
-    select_query = select_template.substitute(values)
-
-    return tryCatchSelectOneQuery(cur, select_query)
-
-def subscriptionExists(email, query):
-    """ Return if subscription exists or not """
-
-    result = getSubscription(email, query)
-    
-    if result is not None:
-        return True
-    else:
-        return False
-
-def subscriberExists(key):
-    """ Return if subscriber exists or not """
-
-    result = getSubscriberByKey(key)
-
-    if result is not None:
-        return True
-    else:
-        return False
-
-def createSubscriber(email):
-    """ Create a subscriber and add to database. Return his unique key """
-
-    values = {
-        'email': email,
-        'key': uuid.uuid4(),
-    }
-
-    insert_template = string.Template("""
-        INSERT INTO "Subscriber" (email, key) VALUES ('$email', '$key')
-    """)
-    insert_query = insert_template.substitute(values)
-    result = tryCatchInsertQuery(insert_query)
-
-    if result is not None:
-        return values['key']
-    else:
-        return None
-
-def createSubscription(email, query):
-    """ Create a subscription and add to database. Return unique id if successful """
-
-    values = {
-        'email':  email,
-        'query': query
-    }
-
-    insert_template = string.Template("""
-        INSERT INTO "Subscription" (subscriber, query) VALUES ('$email', '$query')
-    """)
-    insert_query = insert_template.substitute(values)
-    result = tryCatchInsertQuery(cur, insert_query)
-
-    return result
-
-def updateSubscriberConfirmStatus(key, status):
-    """ Set subscriber with key of key as confirmed """
-
-    values = {
-        'key': key,
-        'status': status
-    }
-
-    update_template = string.Template("""
-        UPDATE "Subscriber" SET verified=$status WHERE key='$token'
-    """)
-    update_query = update_template.substitute(values)
-    return tryCatchInsertQuery(cur, update_query)
 
 def validEmail(email):
     return pattern.match(email)
