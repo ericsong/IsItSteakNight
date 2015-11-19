aad14642868f2e720e321ddfbb1a13c978dce6ae
ericsong <regonics@gmail.com>
2015-11-19 00:14:59 -0500

    add test data creation script

---
+++ server/create_test_data.py
@@ -0,0 +1,56 @@
+from dbinterface import createSubscriber, createSubscription
+
+createSubscriber("regonics+iisntest1@gmail.com")
+createSubscription("regonics+iisntest1@gmail.com", "steak")
+createSubscription("regonics+iisntest1@gmail.com", "rice")
+createSubscription("regonics+iisntest1@gmail.com", "fried chicken")
+
+createSubscriber("regonics+iisntest2@gmail.com")
+createSubscription("regonics+iisntest2@gmail.com", "steak")
+createSubscription("regonics+iisntest2@gmail.com", "salmon")
+
+createSubscriber("regonics+iisntest3@gmail.com")
+createSubscription("regonics+iisntest3@gmail.com", "steak")
+createSubscription("regonics+iisntest3@gmail.com", "rice")
+createSubscription("regonics+iisntest3@gmail.com", "bread")
+
+createSubscriber("regonics+iisntest4@gmail.com")
+createSubscription("regonics+iisntest4@gmail.com", "steak")
+createSubscription("regonics+iisntest4@gmail.com", "rice")
+createSubscription("regonics+iisntest4@gmail.com", "potato")
+
+createSubscriber("regonics+iisntest5@gmail.com")
+createSubscription("regonics+iisntest5@gmail.com", "steak")
+createSubscription("regonics+iisntest5@gmail.com", "lentils")
+createSubscription("regonics+iisntest5@gmail.com", "broccoli")
+createSubscription("regonics+iisntest5@gmail.com", "garlic bread")
+createSubscription("regonics+iisntest5@gmail.com", "fish")
+createSubscription("regonics+iisntest5@gmail.com", "potatoes")
+createSubscription("regonics+iisntest5@gmail.com", "burgers")
+createSubscription("regonics+iisntest5@gmail.com", "sushi")
+createSubscription("regonics+iisntest5@gmail.com", "pesto chicken")
+createSubscription("regonics+iisntest5@gmail.com", "pesto")
+createSubscription("regonics+iisntest5@gmail.com", "garlic")
+createSubscription("regonics+iisntest5@gmail.com", "chowder")
+createSubscription("regonics+iisntest5@gmail.com", "hot dogs")
+createSubscription("regonics+iisntest5@gmail.com", "pho")
+createSubscription("regonics+iisntest5@gmail.com", "ramen")
+createSubscription("regonics+iisntest5@gmail.com", "bacon")
+
+createSubscriber("regonics+iisntest6@gmail.com")
+createSubscription("regonics+iisntest6@gmail.com", "steak")
+createSubscription("regonics+iisntest6@gmail.com", "clam chowder")
+
+createSubscriber("regonics+iisntest7@gmail.com")
+createSubscription("regonics+iisntest7@gmail.com", "steak")
+createSubscription("regonics+iisntest7@gmail.com", "lololol")
+
+createSubscriber("regonics+iisntest8@gmail.com")
+createSubscription("regonics+iisntest8@gmail.com", "steak")
+
+createSubscriber("regonics+iisntest9@gmail.com")
+createSubscription("regonics+iisntest9@gmail.com", "nothing")
+
+createSubscriber("regonics+iisntest10@gmail.com")
+createSubscription("regonics+iisntest10@gmail.com", "steak")
+createSubscription("regonics+iisntest10@gmail.com", "shrimp")
